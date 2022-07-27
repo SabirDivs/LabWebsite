305e3fc9ae2f7bfad179e4484b5d1e394346507c
SabirDivs <108421318+SabirDivs@users.noreply.github.com>
2022-07-27 11:36:28 +0500

    update

---
+++ db.js
@@ -0,0 +1,11 @@
+const db = [
+  (dr_Jamil_Ahmad = {
+    name: "Dr. Jamil Ahmad",
+    phd: "",
+    google_h_index: "Dr. Jamil Ahmad",
+    total_google_citations: "Dr. Jamil Ahmad",
+    total_scopus_citations: "Dr. Jamil Ahmad",
+    orcid: "Dr. Jamil Ahmad",
+    name: "Dr. Jamil Ahmad",
+  }),
+];

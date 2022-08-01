1829622b727bca951b02bc1faa9f3c58ba83c004
SabirDivs <108421318+SabirDivs@users.noreply.github.com>
2022-08-01 15:56:54 +0500

    Update testingTeam.html

--- behaviour.js
+++ behaviour.js
@@ -1,19 +1,22 @@
 // counter
 $(document).ready(function () {
-    
-    $('.counter').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 4000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
+  $(".counter").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
 });
-
 
 // Set date
 const date = document.getElementById("date");
@@ -29,44 +32,44 @@ const navToggler = document.querySelector(".navbar-toggler");
 
 // Accessing window scroll object
 window.addEventListener("scroll", function () {
-// console.log(window.pageYOffset);
-const scrollHeight = window.pageYOffset;
-if (scrollHeight > 600) {
-  toplink.classList.add("show-link");
-} else {
-  toplink.classList.remove("show-link");
-}
+  // console.log(window.pageYOffset);
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 600) {
+    toplink.classList.add("show-link");
+  } else {
+    toplink.classList.remove("show-link");
+  }
 });
 // Smooth Scrolling
 const smoothSroll = document.querySelectorAll(".nav-link");
 smoothSroll.forEach(function (link) {
-link.addEventListener("click", function (e) {
-  // this prevents the #s from navigating to sections by default
-  e.preventDefault();
-  // window.location.assign(e.srcElement.href)
-  // 'srcElement' is deprecated
-  // using event.target property instead
-  window.location.assign(e.target.href)
-  
-  // window.location.replace(e.srcElement.href);
-  // Navigate to specific spot
-  const id = e.currentTarget.getAttribute("href").slice(1);
-  // slice extracts a section of a string without modifying original string
-  const element = document.getElementById(id);
+  link.addEventListener("click", function (e) {
+    // this prevents the #s from navigating to sections by default
+    e.preventDefault();
+    // window.location.assign(e.srcElement.href)
+    // 'srcElement' is deprecated
+    // using event.target property instead
+    window.location.assign(e.target.href);
 
-  // Calculate the heights
-  const navbarHeight = navbar.getBoundingClientRect().height;
-  console.log(navbarHeight);
-  const navbarlinksHeight = navbarlinksDiv.getBoundingClientRect().height;
-  console.log(navbarlinksHeight);
+    // window.location.replace(e.srcElement.href);
+    // Navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    // slice extracts a section of a string without modifying original string
+    const element = document.getElementById(id);
 
-  // offsetTop property give a number represting the top position of the element in pixels
-  let position = element.offsetTop - navbarHeight;
-  window.scrollTo({
-    left: 0,
-    top: position,
+    // Calculate the heights
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    console.log(navbarHeight);
+    const navbarlinksHeight = navbarlinksDiv.getBoundingClientRect().height;
+    console.log(navbarlinksHeight);
+
+    // offsetTop property give a number represting the top position of the element in pixels
+    let position = element.offsetTop - navbarHeight;
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // auto close the navbar when clicked
+    $(".navbar-collapse").collapse("hide");
   });
-  // auto close the navbar when clicked
-  $(".navbar-collapse").collapse("hide");
-});
 });
